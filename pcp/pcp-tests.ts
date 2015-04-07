4490c45fd5df5380c7a006ed2210c0dc9e6b442e
progre <djyayutto@gmail.com>
2015-04-08 00:32:56 +0900

    add test

--- pcp/pcp-tests.ts
+++ pcp/pcp-tests.ts
@@ -1,11 +1,56 @@
-/// <reference path="../mocha/mocha.d.ts" />
+﻿/// <reference path="../mocha/mocha.d.ts" />
 /// <reference path="../power-assert/power-assert.d.ts" />
 /// <reference path="./pcp.d.ts" />
 import assert = require('power-assert');
 import pcp = require('pcp');
+import path = require('path');
 
 describe('pcp',() => {
-    it('exists',() => {
-        assert(pcp != null);
+    it('create server',() => {
+        var id = '00000000000000000000000000000001';
+
+        var server = pcp.createServer();
+        server.listen(7244);
+        server.on('request',(req, res, socket) => {
+            var url = req.url.split('?')[0].split('/');
+            if (url[1] !== 'channel') {
+                res.writeHead(404);
+                res.end();
+                return;
+            }
+            if (url.length < 1 || url[2].length !== 32) {
+                res.writeHead(404);
+                res.end();
+                return;
+            }
+            if (url[2] !== id) {
+                res.writeHead(404);
+                res.end();
+                return;
+            }
+            res.writeHead(200);
+            res.end();
+            socket.end();
+            socket.olleh();
+        });
+
+        return pcp.requestHTTP({
+            hostname: '127.0.0.1',
+            port: 7244,
+            path: '/channel/' + '00000000000000000000000000000000'
+        })
+            .then(res => {
+            assert(res.statusCode === 404);
+            res.socket.end();
+            return pcp.requestHTTP({
+                hostname: '127.0.0.1',
+                port: 7244,
+                path: '/channel/' + id
+            });
+        }).then(res => {
+            assert(res.statusCode === 200);
+            res.socket.end();
+            server.close();
+        });
     });
 });
--- pcp/pcp.d.ts
+++ pcp/pcp.d.ts
@@ -4,19 +4,30 @@
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
 /// <reference path="../node/node.d.ts" />
+/// <reference path="../es6-promise/es6-promise.d.ts" />
 
 declare module "pcp" {
     import events = require("events");
     import http = require("http");
 
-    function createServer();
+    function createServer(): Server;
+    function connectPCP(port: number, host?: string): Promise<PCPSocket>;
+    function requestHTTP(options: any): Promise<{ statusCode: number; socket: PCPSocket; }>;
+
     interface Server extends events.EventEmitter {
         on(event: "connection", listener: (socket: PCPSocket) => void): events.EventEmitter;
         on(event: "request", listener: (req: http.IncomingMessage, res: http.ServerResponse, socket: PCPSocket) => void): events.EventEmitter;
         on(event: "listening", listener: () => void): events.EventEmitter;
         on(event: string, listener: Function): events.EventEmitter;
+        listen(port: number, backlog?: number): Promise<{}>;
+        listen(port: number, hostname: string, backlog: number): Promise<{}>;
+        close(): void;
     }
 
     interface PCPSocket {
+        hello(agentName: string, port: number): void;
+        olleh(): void;
+        quit(): void;
+        end(): void;
     }
 }
